5cf1d0e2f360bc8a69631f45ff87d1979211e433
Dany_Alzate <yesidalzate94@gmail.com>
2023-01-03 09:53:34 -0500

    Desde la CMD de Node

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const deskTopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const burguerMenu = document.querySelector('.menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
+burguerMenu.addEventListener('click', toggleMobileMenu);
 function toggleDesktopMenu() {
     deskTopMenu.classList.toggle('inactive')
+};
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
 
